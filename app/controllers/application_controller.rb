a5effb5f5dc24c8e97da1acce9c019a0761a16d0
Jessica Brand <jeshbrand@gmail.com>
2021-09-13 14:11:09 +0100

    added permission for the nickname param to be passed through at signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    # this allows me to permit new signup parameters, e.g. nickname
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
+
 end
