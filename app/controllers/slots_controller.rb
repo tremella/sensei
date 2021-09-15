b5ba40f497bcef3d0e5962a8d0f7986e151b116c
Jessica Brand <jeshbrand@gmail.com>
2021-09-15 16:21:04 +0100

    student can book slot and SHOW page reflects this - routing still needs work though

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -4,15 +4,15 @@ class SlotsController < ApplicationController
     @slot = Slot.all
   end
 
+
   #only coaches can make a new slot.
   def new
     @slot = Slot.new(coach_id: current_user.id)
   end
   
+  #implements creation
   def create
-
     @slot = Slot.new(slot_params)
-    
     if @slot.save
       redirect_to @slot
     else
@@ -22,7 +22,23 @@ class SlotsController < ApplicationController
   end
   
   def book
-    @slots = Slot.all
+    
+  end
+
+  def edit
+    puts params, 'PARAMS HERE'
+    @slot = Slot.find(params[:id])
+    @slot.update(is_booked: 1, student_id: current_user.id)
+  end
+
+  def update
+    @slot = Slot.find(params[:id])
+
+    if @slot.update(slot_params)
+      redirect_to @slot
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -31,7 +47,9 @@ class SlotsController < ApplicationController
   def show
     @slot = Slot.find(params[:id])
     @coach = User.find(@slot.coach_id)
-    puts @coach.nickname, 'HERE'
+    if @slot.student_id
+      @student = User.find(@slot.student_id)
+    end
   end
 
   private
